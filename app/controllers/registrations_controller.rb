c1f1436fa5348825331870df438e18e6616808bc
drifterz13 <mixmyxxe@gmail.com>
2025-05-31 15:22:04 +0700

    feat: auth

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,24 @@
+class RegistrationsController < ApplicationController
+  allow_unauthenticated_access
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(registration_params)
+
+    if @user.save
+      start_new_session_for @user
+      redirect_to after_authentication_url, notice: "Account created successfully."
+    else
+      redirect_to new_registration_path, alert: @user.errors.full_messages.to_sentence
+    end
+  end
+
+  private
+  
+  def registration_params
+    params.require(:user).permit(:email_address, :password, :password_confirmation)
+  end
+end
